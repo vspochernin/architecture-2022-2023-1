d5f9b01f83cb6c9124d177913a7465e3b257832e
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-15 21:28:10 +0300

    code: initial commit

---
+++ simulation-model/src/main/java/ru/vspochernin/simulationmodel/SimulationModelApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.simulationmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationModelApplication.class, args);
+	}
+
+}
---
+++ simulation-model/src/test/java/ru/vspochernin/simulationmodel/SimulationModelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.simulationmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
